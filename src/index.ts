324e9abd3844bd166499156b1be721455705582d
Clara001M <mariaclara.sm003@gmail.com>
2023-09-19 11:52:09 -0300

    cadastro de pessoas

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,25 @@ const router = Router()
 app.use(express.json()) // Configurando para receber JSON
 app.use(router)
 
+type Pessoa = {
+  nome: string
+  email: string
+}
+const listPessoas: Pessoa [] = []
+
 router.get('/', (request, response) =>
   response.json({ ola: 'Ola mundo', nome: 'Clara' }))
+
+router.post('/formulario', (request, response) => {
+  listPessoas.push({ nome: request.body.nome, email: request.body.email })
+  return response.json().status(201).json({})
+})
+router.get('/carregar', (request, response) => {
+  listPessoas.push({ nome: 'Lucas Ribeiro', email: 'lucasribeiro@gmail.com' })
+  listPessoas.push({ nome: 'Camilo de Sousa', email: 'camilosousa@gmail.com' })
+  listPessoas.push({ nome: 'Maria Clara', email: 'mariaclara@gmail.com' })
+
+  return response.json({ list: listPessoas })
+})
 
 app.listen(3000, () => { console.log('Running port 3000') })
